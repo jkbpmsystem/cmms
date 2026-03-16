b2c9882ae6ed1968c0d9881f423f087fcd6c3aa5
jkbpmsystem <pmsystembackend@gmail.com>
2026-03-16 09:23:23 +0800

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,1 +1,18 @@
+function showLoading(){
+const loader = document.getElementById("loadingScreen");
+if(loader) loader.style.display="flex";
+}
 
+function hideLoading(){
+const loader = document.getElementById("loadingScreen");
+if(loader) loader.style.display="none";
+}
+
+/* auto loading for fetch */
+async function fetchData(url){
+showLoading();
+const res = await fetch(url);
+const data = await res.json();
+hideLoading();
+return data;
+}
